0ca42bc58d00275c7b3ec4cd4a7c3cf2967889a1
Ossama1903 <sheikhusamabilal@gmail.com>
2022-10-01 22:18:55 +0500

    Create Task Functionality done

--- server/controllers/task-controller.js
+++ server/controllers/task-controller.js
@@ -1,6 +1,14 @@
-const addTask = (req, res) => {
-  console.log(req.body);
-  res.json({ msg: "addTask" });
+const Task = require("../models/task");
+
+const addTask = async (req, res) => {
+  const { name } = req.body;
+  const createdTask = new Task({ name });
+  if (createdTask) {
+    await createdTask.save();
+    res.status(200).send("Task Created Successfully");
+  } else {
+    res.status(500).send("Error creating task");
+  }
 };
 
 module.exports = {
